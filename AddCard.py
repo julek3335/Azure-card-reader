8d8968133662cba0dd6f3c9be90ee23fce0f1a97
julek3335 <86291866+julek3335@users.noreply.github.com>
2022-06-22 13:39:01 +0200

    Update AddCard.py

--- AddCard.py
+++ AddCard.py
@@ -11,21 +11,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     with pyodbc.connect(peopleConString) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT cardcode from People where cardcode = %s" % cardCodestring)
-            row = cursor.fetchone()
-            flag = 0
-            while row:
-                if cardCodestring == row[0]:
-                    flag = 1
-                    break
-                row = cursor.fetchone()
-            if flag == 0:
+            try:
+                cursor.execute("SELECT cardcode from People where cardcode = %s" % cardCodestring)
+                return func.HttpResponse("obiekt istnieje w bazie ", status_code=418)
+            
+            except:
                 cursor.execute("insert into People VALUES('%s')" % cardCodestring)
                 return func.HttpResponse("dodano ", status_code=302)
-            if flag == 1:
-                return func.HttpResponse("obiekt istnieje w bazie ", status_code=418)
-
-            
-
-                
-        
