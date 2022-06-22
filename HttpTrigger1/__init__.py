56de5b2609c711e105e6177566cecd58a84c1734
julek3335 <86291866+julek3335@users.noreply.github.com>
2022-06-22 13:38:35 +0200

    Update __init__.py

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -6,24 +6,15 @@ import os
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     cardCode = req.get_json()
-    wartosc = cardCode["cardCode"]
-    cardCodestring = str(wartosc)
-    # logging.info (type(cardCodestring))
+    cardCodestring = str(cardCode["cardCode"])
+
     peopleConString = os.getenv('PeopleConString')
-    # logging.info(f'conString {peopleConString}')
 
     with pyodbc.connect(peopleConString) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT cardcode from People")
-            row = cursor.fetchone()
-            flag = 0
-            while row:
-                # logging.info (str(row[0]))
-                if cardCodestring == row[0]:
-                    flag = 1
-                row = cursor.fetchone()
-
-    if flag == 0:
-        return func.HttpResponse("zla karta", status_code=418)
-    if flag == 1:
-        return func.HttpResponse('Dobra karta', status_code=302)
+            try:
+                cursor.execute("SELECT cardcode from People where cardcode = %s" % cardCodestring)
+                return func.HttpResponse('Dobra karta', status_code=302) 
+                
+            except:
+                return func.HttpResponse("zla karta", status_code=418)
